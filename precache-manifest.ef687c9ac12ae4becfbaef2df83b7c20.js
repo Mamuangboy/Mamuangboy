6e1603b16462a6f159e5d0c5bbef94d385bfaa5e
Mamuangboy <139766967+Mamuangboy@users.noreply.github.com>
2023-07-18 18:17:18 +0700

    BUILD

---
+++ precache-manifest.ef687c9ac12ae4becfbaef2df83b7c20.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab41fe6fdec800387bf6007b157c0ccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a2179926f07e269610",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "17eff890292d4b6d8f88",
+    "url": "/static/css/main.d1120d81.chunk.css"
+  },
+  {
+    "revision": "72a2179926f07e269610",
+    "url": "/static/js/2.2a673c5e.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.2a673c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a63bbd3e7b3309f263d",
+    "url": "/static/js/3.417bf817.chunk.js"
+  },
+  {
+    "revision": "17eff890292d4b6d8f88",
+    "url": "/static/js/main.2cde9c51.chunk.js"
+  },
+  {
+    "revision": "ee2d184a79f6ca1798ea",
+    "url": "/static/js/runtime-main.aacb5fce.js"
+  },
+  {
+    "revision": "9c7253337eedeec26ce1af09b063f770",
+    "url": "/static/media/Mightyspidey.9c725333.ttf"
+  },
+  {
+    "revision": "403b625bfc2468b90bb0d8ea1aef9c00",
+    "url": "/static/media/PrintAble4U-Regular.403b625b.ttf"
+  }
+]);
